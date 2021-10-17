a7a5ca456dc89065112c9319a22a0e6698ca1b9c
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-10-16 22:07:44 -0700

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,13 @@
+int main() {
+	
+// declaring integer
+	int x = 5;
+
+// declaring a pointer
+	int* y = &x;
+
+// * used to reference what is in the pointer
+	bool statement;
+	statement = 5 == *y;
+	cout << statement;
+}
